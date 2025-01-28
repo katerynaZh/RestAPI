cca59e83046cdc73ebf2a85077d1835ec4500ba0
Viktor Dronov <viktor.dronov.ah@gmail.com>
2025-01-28 20:59:42 +0100

    Added first steps to db functionality

--- app/models.py
+++ app/models.py
@@ -1,7 +1,15 @@
 from pydantic import BaseModel, Field
+from app.database import Base
 
 class Task(BaseModel):
     id: int
     title: str = Field(..., max_length=100)
     description: str = Field(None, max_length=300)
     status: str = Field(..., pattern="^(pending|completed)$")
+
+class TaskDB(Base):
+    __tablename__ = "tasks"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    status = Column(String, index=True)
